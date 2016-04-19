bd1395e2ab5be7c80d4bd15dfe9674b298e8306b
hamsterofdark <mlandryviolin@gmail.com>
2016-04-19 18:14:03 -0400

    more angular added

---
+++ src/anrLimited/wwwroot/html/todo.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("myApp", TodoController)
+
+function TodoController($scope) {
+    $scope.totalTodos = 4;
+
+    $scope.todos = [
+	{ text: "learn Angular", status: false },
+	{ text: "buy groceries", status: false }
+    ]
+
+}
