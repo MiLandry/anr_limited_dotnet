fc22762a60ed3de7240cdbbaf44c9b2cdbe6c5ac
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-04-22 18:38:43 -0400

    got decklist working

--- src/anrLimited/wwwroot/html/ng.js
+++ src/anrLimited/wwwroot/html/ng.js
@@ -4,6 +4,11 @@
 //declare and implement controller
 var MyController = function ($scope) {
     $scope.message = "Hello!";
+    
+    $scope.updateResult = function (theData) {
+        console.log("updateResult called")
+        $scope.result = theData;
+    };
 };
 
 app.controller("MyController", MyController); //register
--- src/anrLimited/wwwroot/js/MainController.js
+++ src/anrLimited/wwwroot/js/MainController.js
@@ -1,15 +1,18 @@
 ﻿var MainController = function ($scope) {
     $scope.message = "Hello!";
-    $scope.noise = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01001.png", "Noise");
-    $scope.deckList = [];
+    $scope.noise = noise;
+    $scope.deckList = [noise];
 };
 
 class Card {
-    constructor(imgSrc, name) {
+    constructor(imgSrc, name, quantity) {
         this.imgSrc = imgSrc;
         this.name = name;
+        this.quantity = quantity;
     }
 
 }
+
+noise = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01001.png", "Noise", 1);
 
 app.controller("MainController", MainController);
