0a0f0d8e89c48c1a24dde9119c754865a49e9dac
hamsterofdark <mlandryviolin@gmail.com>
2016-04-22 16:17:04 -0400

    moar work added controllers and an ng playground

---
+++ src/anrLimited/wwwroot/html/ng.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("myApp", []);
+
+
+//declare and implement controller
+var MyController = function ($scope) {
+    $scope.message = "Hello!";
+};
+
+app.controller("MyController", MyController); //register
---
+++ src/anrLimited/wwwroot/js/MainController.js
@@ -0,0 +1,15 @@
+﻿var MainController = function ($scope) {
+    $scope.message = "Hello!";
+    $scope.noise = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01001.png", "Noise");
+    $scope.deckList = [];
+};
+
+class Card {
+    constructor(imgSrc, name) {
+        this.imgSrc = imgSrc;
+        this.name = name;
+    }
+
+}
+
+app.controller("MainController", MainController);
