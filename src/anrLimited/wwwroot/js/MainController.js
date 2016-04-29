aef1a686e0ac9c2b241aa68e5561aa4e2f11e993
hamsterofdark <mlandryviolin@gmail.com>
2016-04-29 03:54:35 -0400

    got a database going

--- src/anrLimited/wwwroot/js/MainController.js
+++ src/anrLimited/wwwroot/js/MainController.js
@@ -1,60 +1,67 @@
-﻿(function () {
-var MainController = function ($scope) {
-
-    $scope.cardOptions = [dataMine, scorchedEarth, neuralKatana];
-
-    $scope.deckList = [noise];
-
-    $scope.selectCard = function (card)
+﻿(function ()
+{
+    var MainController = function ($scope, $cards)
     {
-        list = $scope.deckList;
 
-        var i;
-        for (i = 0; i < list.length; i++) 
+        $cards.initializeDatabase();
+
+        $scope.cardOptions = [dataMine, scorchedEarth, neuralKatana];
+
+        $scope.deckList = [noise];
+
+        $scope.selectCard = function (card)
         {
-            if (list[i] === card) {
-            console.log("card was found");
-            list[i].quantity++;
-            return;
+            list = $scope.deckList;
+
+            var i;
+            for (i = 0; i < list.length; i++)
+            {
+                if (list[i] === card)
+                {
+                    console.log("card was found");
+                    list[i].quantity++;
+                    return;
+                }
             }
-        }
-        
+
             console.log("new card added");
             $scope.deckList.push(card);
-    };
+        };
 
-    $scope.newCards = function (n)
-    {
-        console.log("new cards 4 u");
-        $scope.cardOptions = [];
-        var list = [];
-        var rand = 01001;
-        for (var i = 0; i < n; i++)
+        $scope.newCards = function (n)
         {
-            var card = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01001.png", "random card", 1);
-            list.push(card);
+            console.log("new cards 4 u");
+            $scope.cardOptions = [];
+            var list = [];
+            var rand = 01001;
+            for (var i = 0; i < n; i++)
+            {
+                var card = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01001.png", "random card", 1);
+                list.push(card);
+
+            }
+
+            $scope.cardOptions = list;
 
         }
+    };
 
-        $scope.cardOptions = list;
+    class Card
+    {
+        constructor(imgSrc, name, quantity)
+        {
+            this.imgSrc = imgSrc;
+            this.name = name;
+            this.quantity = quantity;
+        }
 
     }
-};
 
-class Card {
-    constructor(imgSrc, name, quantity) {
-        this.imgSrc = imgSrc;
-        this.name = name;
-        this.quantity = quantity;
-    }
+    noise = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01001.png", "Noise", 1);
+    dataMine = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01076.png", "Data Mine", 1);
+    scorchedEarth = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01099.png", "Scorched Earth", 1);
+    neuralKatana = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01077.png", "Neural Katana", 1);
 
-}
-
-noise = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01001.png", "Noise", 1);
-dataMine = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01076.png", "Data Mine", 1);
-scorchedEarth = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01099.png", "Scorched Earth", 1);
-neuralKatana = new Card("https://netrunnerdb.com/bundles/netrunnerdbcards/images//cards/en//01077.png", "Neural Katana", 1);
-
-app.controller("MainController", MainController);
+    app.controller("MainController", MainController);
 
 })();
--- src/anrLimited/wwwroot/js/cards.js
+++ src/anrLimited/wwwroot/js/cards.js
@@ -4,13 +4,42 @@
     var cards = (function ()
     {
 
+        var fdb = new ForerunnerDB();
+        var db = fdb.db("myDB");
+        var cardCollection = db.collection("card", { primaryKey: "code" });
+
+        //load cards into the database
+        var cards = $.getJSON("../js/cards.json", function (data)
+        {
+            cardCollection.insert(data);
+
+            var myCards = cardCollection.find({
+                quantity: {
+                    "$gt": 0,
+                    "$lt": 5
+                }
+            });
+            //alert("found some cards: " + myCards[5].code);
+
+            //set initial batch
+            var initialBatch = cardCollection.find({
+                type: {
+                    "$eq": "Identity"
+                }
+            });
+
+
+
+        });
+
+
+
         var getIDs = function (faction, number)
         {
 
 
 
         };
-
 
         var getCardBatch = function (number)
         {
@@ -35,6 +64,6 @@
     })();
 
 
-    var module = angular.module("MainController");
+    //var module = angular.module("MainController");
 
 })();
